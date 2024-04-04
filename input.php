3a2154fd5f09affc6667342fcad61fe622c5aeb6
nvan1710 <anvan2004k4@gmail.com>
2024-04-04 19:40:27 +0700

    project asm2

---
+++ input.php
@@ -0,0 +1,112 @@
+<?php
+require_once ('dbhelp.php');
+
+$s_fullname = $s_age = $s_address = $s_MSV='';
+
+if (!empty($_POST)) {
+	$s_id = uniqid();
+
+	if (isset($_POST['fullname'])) {
+		$s_fullname = $_POST['fullname'];
+	}
+
+	if (isset($_POST['age'])) {
+		$s_age = $_POST['age'];
+	}
+
+	if (isset($_POST['address'])) {
+		$s_address = $_POST['address'];
+	}
+
+	if (isset($_POST['id'])) {
+		$s_id = $_POST['id'];
+	}
+	if (isset($_POST['MSV'])) 
+		$s_MSV = $_POST['MSV'];
+
+	$s_fullname = str_replace('\'', '\\\'', $s_fullname);
+	$s_age      = str_replace('\'', '\\\'', $s_age);
+	$s_address  = str_replace('\'', '\\\'', $s_address);
+	$s_id       = str_replace('\'', '\\\'', $s_id);
+	$s_MSV = str_replace('\'', '\\\'', $s_MSV);
+
+	if ($s_id != '') {
+		//update
+		$sql = "UPDATE student SET fullname = '$s_fullname', age = '$s_age', address = '$s_address', MSV ='$s_MSV'  WHERE id = '$s_id'";
+	} else {
+		//insert
+		$sql = "INSERT INTO student(fullname, age, address, MSV) VALUES ('$s_fullname', '$s_age', '$s_address','$s_MSV')";
+	}
+
+	// echo $sql;
+
+	execute($sql);
+
+	header('Location: index.php');
+	die();
+}
+
+$id = '';
+if (isset($_GET['id'])) {
+	$id          = $_GET['id'];
+	$sql         = 'select * from student where id = '.$id;
+	$studentList = executeResult($sql);
+	if ($studentList != null && count($studentList) > 0) {
+		$std        = $studentList[0];
+		$s_fullname = $std['fullname'];
+		$s_age      = $std['age'];
+		$s_address  = $std['address'];
+		$s_MSV = $std['MSV'];
+	} else {
+		$id = '';
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registation Form * Form Tutorial</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h2 class="text-center">Add Student</h2>
+			</div>
+			<div class="panel-body">
+				<form method="post">
+					<div class="form-group">
+					  <label for="usr">Name:</label>
+					  <input type="number" name="id" value="<?=$id?>" style="display: none;">
+					  <input required="true" type="text" class="form-control" id="usr" name="fullname" value="<?=$s_fullname?>">
+					</div>
+					<div class="form-group">
+					  <label for="birthday">Age:</label>
+					  <input type="number" class="form-control" id="age" name="age" value="<?=$s_age?>">
+					</div>
+					<div class="form-group">
+					  <label for="address">Address:</label>
+					  <input type="text" class="form-control" id="address" name="address" value="<?=$s_address?>">
+					  <div MSV="form-group">
+						<label for="MSV">MSV:</label>
+						<input type="text" class="form-control" id="MSV" name="MSV">
+					</div>
+					<button class="btn btn-success">Save</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
